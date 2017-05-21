a530698f327e9d0a29f0747cbef95f8a83232c25
Youssef Sobhy <youssefsobhy22@gmail.com>
2017-05-21 23:57:01 +0200

    Add validation tests for game maker model

--- spec/factories/game_makers.rb
+++ spec/factories/game_makers.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
-  factory :user do
+  factory :game_maker do
     first_name Faker::Name.first_name
     last_name Faker::Name.last_name
     user_name Faker::Cat.name
-    date_of_birth Faker::Date.birthday(15, 70)
     email Faker::Internet.email
-    password 'joey5599'
-    password_confirmation 'joey5599'
+    password 'joey1133'
+    password_confirmation 'joey1133'
   end
 end
