474bdfa9cf2589a52b21621bb648ce73ccf040b8
Youssef Sobhy <youssefsobhy22@gmail.com>
2017-05-21 13:42:28 +0200

    Finish game maker authentication without tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+
+  mount_devise_token_auth_for 'GameMaker', at: 'gamemaker_auth'
+  as :game_maker do
+    # Define routes for GameMaker within this block.
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520211950) do
+ActiveRecord::Schema.define(version: 20170521091453) do
+
+  create_table "game_makers", force: :cascade do |t|
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "first_name",                               null: false
+    t.string   "last_name",                                null: false
+    t.string   "user_name",                                null: false
+    t.string   "email"
+    t.string   "encrypted_password",     default: "",      null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.text     "tokens"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.index ["email"], name: "index_game_makers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_game_makers_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_game_makers_on_uid_and_provider", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "",      null: false
