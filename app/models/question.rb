88493019352a17852d95c33828378c819984e90e
Youssef Sobhy <youssefsobhy22@gmail.com>
2017-05-27 21:29:12 +0200

    Add validation for choices to have at least one true choice

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,13 @@ class Question < ApplicationRecord
 
   validates :question, presence: true
   validates :choices, length: { minimum: 4, maximum: 4 }
+  validate :at_least_one_true_choice
+
+  private
+
+  def at_least_one_true_choice
+    unless choices.any? { |choice| choice.right_choice == true }
+      return errors.add :base, 'Must have at least one true choice'
+    end
+  end
 end
