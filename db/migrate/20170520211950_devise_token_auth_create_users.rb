ed00335d3961a015ad018451b4e94c975d9b37f7
Salma Osama <salma.qayed@gmail.com>
2017-05-31 09:43:42 +0200

    fix inconsistency in migration

--- db/migrate/20170520211950_devise_token_auth_create_users.rb
+++ db/migrate/20170520211950_devise_token_auth_create_users.rb
@@ -4,8 +4,8 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table(:users) do |t|
       ## Required
-      t.string :provider, null: false, default: ''
-      t.string :uid, null: false, default: 'email'
+      t.string :provider, null: false, default: 'email'
+      t.string :uid, null: false, default: ''
 
       ## User Info
       t.string :first_name,         null: false
